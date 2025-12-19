202deb99da64130fd595cae1f7db1190cd724c56
fereidoon khorshidi <ferydoon@gmail.com>
2025-12-19 23:24:19 +0330

    update

---
+++ resume_auto_review/src/llm_call.py
@@ -0,0 +1,24 @@
+import os
+from openai import OpenAI
+
+api_key = os.environ.get('OPENROUTER_API_KEY')
+if not api_key:
+    raise RuntimeError('OPENROUTER_API_KEY not set in environment')
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=api_key,
+)
+def call_llm(prompt , model="mistralai/devstral-2512:free"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+if __name__ == "__main__":
+    test_prompt = "what is the capital of iraq"
+    print(call_llm(test_prompt))
